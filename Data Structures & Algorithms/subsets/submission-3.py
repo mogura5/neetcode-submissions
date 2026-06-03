ee004571ce86da9301306c7ae10d26d76e406c84
Manaka Ogura <125702450+mogura5@users.noreply.github.com>
2026-06-03 16:48:04 -0400

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [] # final list of all subsets
+        subset = [] # current subset being built
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            
+            subset.append(nums[i])
+            dfs(i + 1)
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0) # 0 is the starting index
+        return res
