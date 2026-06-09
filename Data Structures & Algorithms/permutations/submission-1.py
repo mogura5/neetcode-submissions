f0deca97a0c585596e32d7a8c22ec47369a36bf6
Manaka Ogura <125702450+mogura5@users.noreply.github.com>
2026-06-09 17:32:14 -0400

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        self.output = []
+        self.backtrack([], nums, [False] * len(nums))
+        return self.output
+    
+    def backtrack(self, subset, nums, pick):
+
+        # base case
+        if len(subset) == len(nums):
+            self.output.append(subset.copy())
+            return
+        
+        # loop
+        for i in range(len(nums)):
+
+            if not pick[i]:
+
+                subset.append(nums[i])
+                pick[i] = True
+                self.backtrack(subset, nums, pick)
+
+                # not include
+                subset.pop()
+                pick[i] = False
+        
